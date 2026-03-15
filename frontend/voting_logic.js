8881a05509248eab70430aa2310ae838b5812c39
namburikrishnaganesh <namburikrishnaganesh@gmail.com>
2026-03-15 19:19:51 +0530

    update backend URL to render

--- frontend/voting_logic.js
+++ frontend/voting_logic.js
@@ -3,7 +3,7 @@
 // All fetch requests point to this Flask server address
 // Change this if the backend runs on a different port or host
 // ============================================================
-const backend = "http://127.0.0.1:5000"
+const backend = "https://web3-voting-api.onrender.com"
 
 // ============================================================
 // TOAST NOTIFICATION
